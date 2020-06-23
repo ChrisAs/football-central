17dd1397144767b7bdd9816ce1a01414775bb4a8
ChrisAs <zari87@live.com>
2020-06-23 11:24:59 -0400

    Minor changes

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -5,7 +5,7 @@ import {
   GET_ALL_POST_FAIL,
   DELETE_POST_SUCCESS,
   CREATE_COMMENT_SUCCESS,
-  CREATE_COMMENT_FAIL
+  CREATE_COMMENT_FAIL,
 } from "../actions/postActions";
 
 const initState = {
@@ -14,12 +14,11 @@ const initState = {
 
 export default function postReducer(state = initState, action) {
   switch (action.type) {
-    case CREATE_POST_SUCCESS:{
-
+    case CREATE_POST_SUCCESS: {
       let post = action.payload;
       let posts = state.posts;
       posts.push(post);
-      
+
       return {
         ...state,
         posts,
@@ -42,47 +41,46 @@ export default function postReducer(state = initState, action) {
 
     case GET_ALL_POST_FAIL:
       return state;
-      
-    case DELETE_POST_SUCCESS:{
+
+    case DELETE_POST_SUCCESS: {
       let postIdToDelete = action.payload;
-      let newPosts = state.posts.filter(({_id}) => _id !== postIdToDelete);
-      
+      let newPosts = state.posts.filter(({ _id }) => _id !== postIdToDelete);
+
       return {
         ...state,
-        posts: newPosts
+        posts: newPosts,
       };
     }
 
     case CREATE_COMMENT_SUCCESS: {
-      console.log("PAYLOAD: ", action.payload)
-      console.log("INITIAL STATE: ", state)
+      console.log("PAYLOAD: ", action.payload);
+      console.log("INITIAL STATE: ", state);
       let comment = action.payload.comment;
       let filteredpost = state.posts.map((post) => {
-        console.log("SINGLE PSOT:", post)
-        if(post._id === action.payload.id) {
+        console.log("SINGLE PSOT:", post);
+        if (post._id === action.payload.id) {
           let comments = post.comments;
           comments.push(comment);
 
-          console.log("All Comments: ", comments)
+          console.log("All Comments: ", comments);
           return {
             ...post,
-            comments
+            comments,
           };
         } else {
           return post;
         }
       });
-      console.log("filteredpost: ", filteredpost)
+      console.log("filteredpost: ", filteredpost);
 
       return {
         ...state,
-        posts: filteredpost
-      }
+        posts: filteredpost,
+      };
     }
 
     case CREATE_COMMENT_FAIL:
-      return state;  
-
+      return state;
 
     default:
       return state;
